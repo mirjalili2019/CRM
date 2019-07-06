0f828324efe6fd0fceb57ce1f1d836e6b895e142
mirjalili2019 <fh.mirjalili@gmail.com>
2019-07-06 14:07:59 +0430

    98/04/15
    change design

--- app/src/main/java/com/example/crmproject/activity/login.java
+++ app/src/main/java/com/example/crmproject/activity/login.java
@@ -11,32 +11,32 @@ import android.widget.EditText;
 import com.example.crmproject.R;
 
 public class login extends AppCompatActivity {
-//    EditText edt_user,edt_pass;
-//    CheckBox chk_save;
-//    Button btn_save,btn_pass;
+    EditText edt_user,edt_pass;
+    CheckBox chk_save;
+    Button btn_save,btn_enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-//        initilize();
-//        ////////sabtnam///////////
-//        btn_save.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i=new Intent(login.this, register.class);
-//                startActivity(i);
-//            }
-//        });
+        initilize();
+        ////////sabtnam///////////
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(login.this, register.class);
+                startActivity(i);
+            }
+        });
     }
 
 
-//    private void initilize(){
-//        edt_user=findViewById(R.id.edt_user);
-//        edt_pass=findViewById(R.id.edt_pass);
-//        chk_save=findViewById(R.id.chk_save);
-//        btn_save=findViewById(R.id.btn_save);
-//        btn_pass=findViewById(R.id.btn_save);
-//
-//    }
+    private void initilize(){
+        edt_user=findViewById(R.id.edt_user);
+        edt_pass=findViewById(R.id.edt_pass);
+        chk_save=findViewById(R.id.chk_save);
+        btn_save=findViewById(R.id.btn_save);
+        btn_enter=findViewById(R.id.btn_enter);
+
+    }
 }
